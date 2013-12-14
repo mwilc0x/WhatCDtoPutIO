a56164a78dde8941ffdb3e9290533402b3d456da
Mike Wilcox <mwilcox56@gmail.com>
2013-12-13 22:06:11 -0500

    need to curl torrent to get right path to add to put.io

--- what-cd.js
+++ what-cd.js
@@ -1,37 +1,58 @@
 var WhatCD = require("whatcd");
 var PutIO = require('put.io-v2');
 var sleep = require('sleep');
+var http = require('http');
 
-var api = new PutIO("PUTIO-KEY-HERE");
+var api = new PutIO("TOKEN-HERE");
 var client = new WhatCD("https://what.cd", "USERNAME", "PASSWORD");
 
+
+
 client.top10_torrents(function(err, data) {
-  if(err) {
-    return console.log(err);
-  }
- 
-  for(var i = 0; i < data.length; i++) {
-    for(var j = 0; j < data[i].results.length; j++) {
-      timeout(client, data[i].results[j]);
+    if(err) {
+        return console.log(err);
     }
-  }
+
+    for(var i = 0; i < data.length; i++) {
+        for(var j = 0; j < data[i].results.length; j++) {
+            timeout(client, data[i].results[j]);
+        }
+    }
 });
 
 function timeout(client, data) {
-  client.torrent({id: data.torrentId}, function(err,data){
-    if(err) {
-      return console.log(err);
-    }
-    console.log(data.torrent.id);
-    api.transfers.add('https://what.cd/torrents.php?action=download&id='+ data.torrent.id+'&authkey=6463b1c5ee3ae28c9851e9889c128336&torrent_pass=zuleuwqrjeadavd29a3n0vif5v3i4d47', function(res) {
-      console.log(res);
+    client.torrent({id: data.torrentId}, function(err,data){
+        if(err) {
+            return console.log(err);
+        }
+        console.log(data.torrent.id);
+
+
+
+        api.transfers.add('https://what.cd/torrents.php?action=download&id='+ data.torrent.id+'&authkey=6463b1c5ee3ae28c9851e9889c128336&torrent_pass=zuleuwqrjeadavd29a3n0vif5v3i4d47', function(res) {
+            console.log(res);
+        });
+        sleep.sleep(2);
     });
-    sleep.sleep(2);
-  });
+
+
+    var options = {
+        host: 'what.cd',
+        port: 80,
+        path: '/torrents.php?action=download&id='+data.torrentId
+    };
+
+    http.get(options, function(resp){
+        resp.on('data', function(chunk){
+            console.log(resp);
+        });
+    }).on("error", function(e){
+            console.log("Got error: " + e.message);
+        });
 }
 
 api.files.list(0, function(data) {
-  for(var i in data.files) {
-    //console.log(data.files[i].name);
-  }
+    for(var i in data.files) {
+        //console.log(data.files[i].name);
+    }
 });
